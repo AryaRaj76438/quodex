f4bcbc4630cbca856a6ca532b9df6add3616b061
Rajneesh Kumar <192125aryaraj@gmail.com>
2025-07-01 22:52:05 +0530

    initial files

---
+++ src/main/java/com/qodex/QodexApplication.java
@@ -0,0 +1,18 @@
+package com.qodex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+
+@SpringBootApplication
+public class QodexApplication {
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+		SpringApplication.run(QodexApplication.class, args);
+	}
+
+}
